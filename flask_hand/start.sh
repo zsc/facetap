b2cadcd04b390df2f1a648509a6b93e9fcf0f37b
liuweizhou <liuweizhou@megvii.com>
2022-10-07 17:52:46 +0800

    fix start script

--- flask_hand/start.sh
+++ flask_hand/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #[[ -x /dev/ttyACM0 ]] || sudo chmod 777 /dev/ttyACM0
-cd /home/ubuntu/facetap/flask/
-FLASK_APP=/home/ubuntu/facetap/flask/main.py python2 -m flask run --host=0.0.0.0
+cd /home/dev/facetap/flask_hand/
+FLASK_APP=/home/dev/facetap/flask_hand/main.py python3 -m flask run --host=0.0.0.0
